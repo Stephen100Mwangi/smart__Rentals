23645b2949a90e5886e6911a91d06001477f2648
stephen100mwangi <mwangiwahome70@gmail.com>
2024-02-28 19:21:46 +0300

    Added user model

---
+++ server/Models/UserModel.js
@@ -0,0 +1,24 @@
+import { timeStamp } from 'console'
+import mongoose from 'mongoose'
+
+const UserSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        required: true,
+        min: 3,
+        unique: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    password:{
+        type: String,
+        required: true
+    }
+},{timeStamp:true})
+
+
+const User = mongoose.model("User",UserSchema);
+export default User
