7df7caadb15dc0a8eed8aebb0672ff5f5e3a667c
stephen100mwangi <mwangiwahome70@gmail.com>
2024-02-28 19:12:44 +0300

    Add connection to database

--- server/index.js
+++ server/index.js
@@ -1,7 +1,21 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import { error } from 'console';
+
+dotenv.config();
+
+// Check whether connected
+mongoose.connect(process.env.MONGO_CONNECTION).then(() => {  
+    console.log("Connected to MogoDB");
+}).catch((error)=>{
+    console.log(error);
+})
+
+
 
 // Initialize app
 const app = express();
-app.listen(3055,()=>{
-    console.log("Server running on port 3055");
+app.listen(process.env.PORT || 3455,()=>{
+    console.log(`Server running on port http://localhost:${process.env.PORT}`);
 })
